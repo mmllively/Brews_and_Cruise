2cc258864a8bc85e174d04a70d4895c49e9ca52b
Squiddy <wolaki96@gmail.com>
2022-10-17 09:47:04 -0400

    added button for favorites and links for breweries

--- script.js
+++ script.js
@@ -9,23 +9,73 @@ function GetInfo() {
       .then((completedata) => {
           let data1 = "";
           completedata.map((values) => {
-              data1 += `<div class="card">
-              <h3 class="title">${values.name}</h3 >
-              <p class="brewerytype"><b>Brewery Type:</b> ${values.brewery_type}</p>
-              <p class="websiteurl">${values.website_url}</p>
-              <p class="address">${values.street}</p>
-              <p class="city">${values.city}, ${values.state}  ${values.postal_code}</p>
-              <p class="lat">Latitude: ${values.latitude}</p>
-              <p class="lon">Longitude: ${values.longitude}</p>
-              </div > `;
-          });
+            console.log(completedata);
+            if(values.latitude !== null|| values.longitude !==null){
+            data1 += `<div class="card">
+            <h3 class="title">  ${values.name}  <button id="favorite" onclick="saveFavorites()"
+            <i class="fa-solid fa-beer-mug-empty"></i></button></h3 >
+            <p class="city">${values.phone}</p>
+            <p class="brewerytype"><b>Brewery Type:</b> ${values.brewery_type}</p>
+            <a class="websiteurl"  href=${values.website_url} >${values.website_url}</a>
+            <p class="address">${values.street}</p>
+            <p class="city">${values.city}, ${values.state}  ${values.postal_code}</p>
+            <a href="map.html"  id="mapBtn" data-lon=${values.longitude} data-lat=${values.latitude}>Directions</a>
+            </div > `;
+        }
+        });
+        document.getElementById("card").innerHTML = data1;
+
           document.getElementById("card").innerHTML = data1;
+          document.getElementById("map","brewMap").classList.remove("hide");
+
 
       })
       .catch((err) => {
           console.log(err);
       })
+     
 }
+//local storage for saving favorite breweries
+
+// function saveFavorites() {
+// console.log("did this work?");
+// var title = $(this).siblings(".card").val();
+// var saved = $(this).parent().attr("id");
+// localStorage.setItem(title, saved);
+// }
+
+
+
+
+function initMap(){
+  //Map options
+ 
+  //New map
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: {lat:42.3601, lng: -71.0589},
+    mapID: 'b97d8f64f7d407f6',
+    
+  });
+  console.log(mapID);
+  
+};
+console.log(map);
+initMap();
+
+var service = new google.maps.places.PlacesService(map);
+
+service.findPlaceFromQuery(request, function(results, status){
+  if (status === google.maps.places.PlacesServiceStatus.OK){
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+    map.setCenter(results[0].geometry.location);
+  }
+});
+
+
+
 
 
 
